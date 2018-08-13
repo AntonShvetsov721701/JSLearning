3f08f6f4ac8d0d72085f82249cdf51e600cdb306
AntonShvetsov721701 <34772155+AntonShvetsov721701@users.noreply.github.com>
2018-08-13 14:24:14 +0300

    Update task5.js
    Added changes

--- if_tasks/task5/task5.js
+++ if_tasks/task5/task5.js
@@ -1,4 +1,6 @@
 var age = prompt("Input data ",'');
+
 if(age >= 14 && age <= 90){
     alert("14 < age < 90 - true");
-}else alert('14 < age < 90 - false')
+}else{
+    alert('14 < age < 90 - false')}
